8dea28c47355347dc9f51c9c9a40351caf0b26b2
zxzsz <38557901+zxzsz@users.noreply.github.com>
2020-04-26 22:43:21 +0800

    alias

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+// has been set in webpack.base.conf with an alias.
+// has been set in webpack.base.conf with an alias.
+// has been set in webpack.base.conf with an alias.
 import App from './App'
+// has been set in webpack.base.conf with an alias.
 
 Vue.config.productionTip = false
 
@@ -11,5 +15,8 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   components: { App },
+  // has been set in webpack.base.conf with an alias.
   template: '<App/>'
+  // has been set in webpack.base.conf with an alias.
+  // has been set in webpack.base.conf with an alias.
 })
